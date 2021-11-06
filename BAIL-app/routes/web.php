cee0c17364bc557aa81da304892cbcd2639cafbf
Selim1710 <mdselimhossainsuhag@gmail.com>
2021-11-06 13:57:12 +0600

    making UserController

--- BAIL-app/routes/web.php
+++ BAIL-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('users.content');
-});
+// Route::get('/', function () {
+//     return view('users.content');
+// });
+
+
+
+// User Interface
+
+Route::get('/', [UsersController::class, 'index']);
+
