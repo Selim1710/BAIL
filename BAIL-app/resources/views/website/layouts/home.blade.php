42e6d466ccb44db7dde39d0affec144ef3a27d3b
Selim1710 <mdselimhossainsuhag@gmail.com>
2021-12-14 20:52:57 +0600

    Redesign the accessories and car card and website

--- BAIL-app/resources/views/website/layouts/home.blade.php
+++ BAIL-app/resources/views/website/layouts/home.blade.php
@@ -7,6 +7,31 @@
 
 
 
+
+<!-- just for making the website beautiful -->
+
+
+
+
+
+<section class="client_section layout_padding-bottom">
+  <div class="container">
+    <div class="heading_container heading_center psudo_white_primary mb_45">
+      <h2>
+        <br>
+        <br>
+        Bangladesh Auto Industries Limited
+      </h2>
+    </div>
+    
+  </div>
+</section>
+
+
+<!-- end the design  -->
+
+
+
 <!-- Accessories -->
 <section class="accessories_section layout_padding-bottom">
   <div class="accessories_container">
@@ -16,10 +41,11 @@
           Accessories
         </h2>
       </div>
-      @foreach($accessories as $accessory)
-   
+
       <div class="row">
-        <div class="col-md-6  ">
+        @foreach($accessories as $accessory)
+        <div class="col-md-6">
+
           <div class="box ">
             <div class="img-box">
               <img src="{{ asset('uploads/accessories/'.$accessory->acc_img) }}" alt="">
@@ -37,65 +63,14 @@
               <h6>
                 <span>20%</span> Off
               </h6>
-              <a href="#">
-                Order Now <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
-                  <g>
-                    <g>
-                      <path d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248
-                      c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />
-                    </g>
-                  </g>
-                  <g>
-                    <g>
-                      <path d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48
-                      C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064
-                      c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4
-                      C457.728,97.71,450.56,86.958,439.296,84.91z" />
-                    </g>
-                  </g>
-                  <g>
-                    <g>
-                      <path d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296
-                      c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />
-                    </g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                </svg>
-              </a>
+              <a href="#" class="btn btn-primary">Order Now</a>
             </div>
           </div>
+
         </div>
+        @endforeach
       </div>
-    @endforeach
+
     </div>
   </div>
 </section>
@@ -109,6 +84,7 @@
         Our Product
       </h2>
     </div>
+
     <ul class="filters_menu">
       <li class="active" data-filter="*">All</li>
       <li data-filter=".car">Car</li>
@@ -116,11 +92,14 @@
       <li data-filter=".bike">Bike</li>
       <li data-filter=".truck">Truck</li>
     </ul>
-    @foreach($products as $car)
+
     <div class="filters-content">
       <div class="row grid">
+        @foreach($products as $car)
         <div class="col-sm-6 col-lg-4 all car">
+
           <div class="box">
+
             <div>
               <div class="img-box">
                 <img src="{{url('/uploads/product/'.$car->image_path)}}" alt="">
@@ -139,70 +118,20 @@
                   <h6>
                     Price
                   </h6>
-                  <a href="#">
-                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
-                      <g>
-                        <g>
-                          <path d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248
-                          c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />
-                        </g>
-                      </g>
-                      <g>
-                        <g>
-                          <path d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48
-                          C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064
-                          c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4
-                          C457.728,97.71,450.56,86.958,439.296,84.91z" />
-                        </g>
-                      </g>
-                      <g>
-                        <g>
-                          <path d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296
-                          c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />
-                        </g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                      <g>
-                      </g>
-                    </svg>
-                  </a>
                 </div>
+                <a href="#" class="btn btn-primary">Order Now</a>
               </div>
             </div>
+
           </div>
         </div>
+        @endforeach
       </div>
     </div>
-    @endforeach
+
+
     <!-- bus -->
-    
+
     <div class="btn-box">
       <a href="#">
         View More
